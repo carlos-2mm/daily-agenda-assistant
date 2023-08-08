2f48bff8dec3b8db5373fbc78e767fe469373388
Carlos Macias <cimm21187@gmail.com>
2023-08-08 12:44:28 -0400

    Integrate Bootstrap styling and modified JavaScript to utilize Bootstrap's grid system, form controls, and button styles.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,28 @@ $(document).ready(function() {
   const timeBlocks = $("#timeBlocks");
   $("#currentDay").text(new Date().toDateString());
 
+  function saveToLocalStorage(hour, value) {
+      localStorage.setItem(hour, value);
+  }
+
   for (let i = 9; i <= 17; i++) {
       const hourLabel = i <= 12 ? `${i} AM` : `${i-12} PM`;
       const eventValue = localStorage.getItem(i.toString()) || "";
 
       // create a row for each hour
-      const row = $(`<div></div>`);
+      const row = $(`<div class="row align-items-center mb-2"></div>`);
       
       // hour label
-      row.append(`<div>${hourLabel}</div>`);
+      row.append(`<div class="col-2 text-right">${hourLabel}</div>`);
 
       // input field
-      const inputField = $(`<input value="${eventValue}">`);
+      const inputField = $(`<input class="form-control col-8" value="${eventValue}">`);
       row.append(inputField);
 
       // save button
-      const saveBtn = $(`<button>Save</button>`);
+      const saveBtn = $(`<button class="btn btn-primary col-2"></button>`);
+      const saveIcon = $("<i>").addClass("fa-solid fa-floppy-disk");
+      saveBtn.append(saveIcon)
       saveBtn.on("click", function() {
           localStorage.setItem(i.toString(), inputField.val());
       });
@@ -27,11 +33,11 @@ $(document).ready(function() {
       // color coding
       const currentHour = new Date().getHours();
       if (i < currentHour) {
-          inputField.addClass();
+          inputField.addClass('past');
       } else if (i === currentHour) {
-          inputField.addClass();
+          inputField.addClass('present');
       } else {
-          inputField.addClass();
+          inputField.addClass('future');
       }
 
       timeBlocks.append(row);
